5344644c5dc9a254def91235cd2c9eb32883e829
davidrgb <davidrussellrgb@gmail.com>
2021-02-01 13:43:22 -0600

    Finished PiggyBank

--- view/PiggyBankSimulator.java
+++ view/PiggyBankSimulator.java
@@ -14,11 +14,18 @@ import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
+import controller.PBHistoryListener;
+import controller.PBInsertListener;
+import controller.PBStatsListener;
+import model.Coin;
+import model.PiggyBank;
+
 public class PiggyBankSimulator {
 
     private JFrame window;
     private JTextArea display = new JTextArea();
     private JRadioButton[] radioButtons;
+    private PiggyBank piggyBank = new PiggyBank();
     
     public PiggyBankSimulator(JFrame window) {
         this.window = window;
@@ -39,10 +46,10 @@ public class PiggyBankSimulator {
         cp.add(BorderLayout.SOUTH, southPanel);
 
         radioButtons = new JRadioButton[3];
-        radioButtons[0] = new JRadioButton("Nickel");
+        radioButtons[0] = new JRadioButton(Coin.NICKEL);
         radioButtons[0].setSelected(true);
-        radioButtons[1] = new JRadioButton("Dime");
-        radioButtons[2] = new JRadioButton("Quarter");
+        radioButtons[1] = new JRadioButton(Coin.DIME);
+        radioButtons[2] = new JRadioButton(Coin.QUARTER);
         
         JPanel radioPanel = new JPanel();
         ButtonGroup radioGroup = new ButtonGroup();
@@ -54,17 +61,39 @@ public class PiggyBankSimulator {
         southPanel.add(radioPanel);
 
         var insertButton = new JButton("Insert");
+        insertButton.addActionListener(new PBInsertListener(this));
         var historyButton = new JButton("History");
+        historyButton.addActionListener(new PBHistoryListener(this));
         var statsButton = new JButton("Stats");
+        statsButton.addActionListener(new PBStatsListener(this));
         var exitButton = new JButton("Exit");
+        exitButton.addActionListener(e -> {
+            window.getContentPane().removeAll();
+            var menu = new MenuScreen(window);
+            menu.init();
+            window.pack();
+            window.revalidate();
+        });
         
         JPanel buttonPanel = new JPanel();
         southPanel.add(buttonPanel);
-        
+
         buttonPanel.add(insertButton);
         buttonPanel.add(historyButton);
         buttonPanel.add(statsButton);
         buttonPanel.add(exitButton);
 
+    }
+
+    public PiggyBank getPiggyBank() {
+        return piggyBank;
+    }
+
+    public JTextArea getDisplay() {
+        return display;
+    }
+
+    public JRadioButton[] getRadioButtons() {
+        return radioButtons;
     }
 }
